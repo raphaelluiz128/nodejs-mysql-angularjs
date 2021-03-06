6e6126031e7f79a6487cb3d20b2a475479d47523
raphaelluizschettino <raphaelluiz128@hotmail.com>
2021-03-06 17:16:18 -0300

    mofidy routes

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const tasksRoutes = require('./routes/tasks');
 app.use('/users', usersRoutes);
 app.use('/tasks', tasksRoutes);
 
+app.use((req, res, next) => {
+    const erro = new Error("Não encontrado");
+    erro.status = 404;
+    next(erro);
+});
 
+app.use((error, req, res ,next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    })
+});
 
 module.exports = app;

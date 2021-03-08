66a4a7aa019126d961d9c7a62b9a0644d46d17d3
raphaelluizschettino <raphaelluiz128@hotmail.com>
2021-03-08 03:54:39 -0300

    add field to table tasks to making count of come back to peding

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -21,7 +21,7 @@ exports.create = async (req, res, next) => {
                  });
              }
                 let dataTask = {id: result.insertId, responsibleEmail: task.responsibleEmail, 
-                    responsible: task.responsible, description: task.description, status: task.status };
+                    responsible: task.responsible, description: task.description, status: task.status, comeToPeding: 0 };
                 return res.status(201).send({  
                  message: 'Tarefa criada com sucesso',
                  idTask: result.insertId,
@@ -43,11 +43,12 @@ exports.update = async (req, res, next) => {
         responsible: req.body.name,
         responsibleEmail: req.body.email,
         description: req.body.description,
-        status: req.body.status,   
+        status: req.body.status, 
+        comeToPeding: req.body.comeToPeding  
     };
    
     mysql.getConnection((error, conn) => {
-        conn.query('UPDATE tasks SET responsible = ?, responsibleEmail = ?, description = ?, status = ? comeToPedingwhere id = ?',
+        conn.query('UPDATE tasks SET responsible = ?, responsibleEmail = ?, description = ?, status = ?, comeToPeding = ? where id = ?',
          [task.responsible,task.responsibleEmail,task.description,task.status, task.comeToPeding, req.params.id], (error, result, field) => {
              conn.release();
              if(error){
